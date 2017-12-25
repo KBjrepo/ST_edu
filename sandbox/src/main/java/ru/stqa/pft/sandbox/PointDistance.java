24243aa847a893e54ea1b174d576ea3b96fd12bd
KBjrepo <34812298+KBjrepo@users.noreply.github.com>
2017-12-25 23:56:45 +0300

    Добавил передачу параметров второй точки в виде объекта в метод

--- sandbox/src/main/java/ru/stqa/pft/sandbox/PointDistance.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/PointDistance.java
@@ -4,8 +4,9 @@ public class PointDistance {
   public static void main(String[] args) {
 
     Point p1 = new Point(0.5,1);
+    Point p2 = new Point(2, 3);
 
-    System.out.println("Расстояние между двумя точками = " + p1.distance(2, 3));
+    System.out.println("Расстояние между двумя точками = " + p1.distance(p2));
   }
 
 
